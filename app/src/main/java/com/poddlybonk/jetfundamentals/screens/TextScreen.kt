47940d49d0142d5c07685902c5a937164f6942fb
Simon Rofe <simon.rofe@ieee.org>
2022-06-11 12:09:41 +1000

    Add text composable to Text Screen

--- app/src/main/java/com/poddlybonk/jetfundamentals/screens/TextScreen.kt
+++ app/src/main/java/com/poddlybonk/jetfundamentals/screens/TextScreen.kt
@@ -3,9 +3,12 @@ package com.poddlybonk.jetfundamentals.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.poddlybonk.jetfundamentals.R
 import com.poddlybonk.jetfundamentals.router.BackButtonHandler
 import com.poddlybonk.jetfundamentals.router.JetFundamentalsRouter
 import com.poddlybonk.jetfundamentals.router.Screen
@@ -27,5 +30,5 @@ fun TextScreen() {
 
 @Composable
 fun MyText() {
-    // TODO add your code here
+    Text(text = stringResource(id = R.string.jetpack_compose))
 }
