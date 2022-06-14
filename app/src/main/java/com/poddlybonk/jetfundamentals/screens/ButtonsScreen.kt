d339e038ba61d049c59a992a61927cef67f4509b
Simon Rofe <simon.rofe@ieee.org>
2022-06-14 11:43:51 +1000

    Add radio button group to Button Screen

--- app/src/main/java/com/poddlybonk/jetfundamentals/screens/ButtonsScreen.kt
+++ app/src/main/java/com/poddlybonk/jetfundamentals/screens/ButtonsScreen.kt
@@ -4,15 +4,16 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.poddlybonk.jetfundamentals.R
 import com.poddlybonk.jetfundamentals.router.BackButtonHandler
@@ -35,6 +36,7 @@ fun ExploreButtonsScreen() {
 }
 
 @Composable
+@Preview
 fun MyButton() {
     Button(
         onClick = {},
@@ -53,7 +55,25 @@ fun MyButton() {
 
 @Composable
 fun MyRadioGroup() {
-    // TODO add your code here
+    val radioButtons = listOf(0, 1, 2)
+    val selectedButton = remember { mutableStateOf((radioButtons.first())) }
+    
+    Column {
+        radioButtons.forEach { index -> 
+            val isSelected = index == selectedButton.value
+            val colors = RadioButtonDefaults.colors(
+                selectedColor = colorResource(id = R.color.colorPrimary),
+                unselectedColor = colorResource(id = R.color.colorPrimaryDark),
+                disabledColor = Color.LightGray
+            )
+
+            RadioButton(
+                colors = colors,
+                selected = isSelected,
+                onClick = { selectedButton.value = index }
+            )
+        }
+    }
 }
 
 @Composable
