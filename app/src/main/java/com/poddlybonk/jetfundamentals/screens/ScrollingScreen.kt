e26dd3568d5453d63ee15b466eb24110d7436fb4
Simon Rofe <simon.rofe@ieee.org>
2022-06-23 16:02:00 +1000

    Change to horizontal scrolling screen

--- app/src/main/java/com/poddlybonk/jetfundamentals/screens/ScrollingScreen.kt
+++ app/src/main/java/com/poddlybonk/jetfundamentals/screens/ScrollingScreen.kt
@@ -3,7 +3,9 @@ package com.poddlybonk.jetfundamentals.screens
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -30,7 +32,7 @@ fun ScrollingScreen() {
 
 @Composable
 fun MyScrollingScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
         BookImage(R.drawable.advanced_architecture_android, R.string.advanced_architecture_android)
         BookImage(R.drawable.kotlin_aprentice, R.string.kotlin_apprentice)
         BookImage(R.drawable.kotlin_coroutines, R.string.kotlin_coroutines)
