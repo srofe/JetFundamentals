31a18c988f59ea6b72e84440cfc1b1aa84280209
Simon Rofe <simon.rofe@ieee.org>
2022-06-23 15:08:27 +1000

    Add grid screen for starter project

---
+++ app/src/main/java/com/poddlybonk/jetfundamentals/screens/GridScreen.kt
@@ -0,0 +1,49 @@
+package com.poddlybonk.jetfundamentals.screens
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.poddlybonk.jetfundamentals.router.BackButtonHandler
+import com.poddlybonk.jetfundamentals.router.JetFundamentalsRouter
+import com.poddlybonk.jetfundamentals.router.Screen
+
+private val items = listOf(
+    Icons.Filled.Check,
+    Icons.Filled.Close,
+    Icons.Filled.ThumbUp,
+    Icons.Filled.Build,
+    Icons.Filled.Delete,
+    Icons.Filled.Home,
+    Icons.Filled.Close,
+    Icons.Filled.ThumbUp,
+    Icons.Filled.Build,
+    Icons.Filled.ThumbUp,
+)
+
+@Composable
+fun GridScreen() {
+    GridView(columnCount = 3)
+
+    BackButtonHandler {
+        JetFundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+fun GridView(columnCount: Int) {
+    // TODO add your code here
+}
+
+@Composable
+fun RowItem(rowItems: List<IconResource>) {
+    // TODO add your code here
+}
+
+@Composable
+fun RowScope.GridIcon(iconResource: IconResource) {
+    // TODO add your code here
+}
+
+data class IconResource(val imageVector: ImageVector, val isVisible: Boolean)
