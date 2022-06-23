c0dcd52528f4f0bdd89df91055c7e03fbfcccb68
Simon Rofe <simon.rofe@ieee.org>
2022-06-23 15:58:23 +1000

    Add a vertical scrolling screen

--- app/src/main/java/com/poddlybonk/jetfundamentals/screens/ScrollingScreen.kt
+++ app/src/main/java/com/poddlybonk/jetfundamentals/screens/ScrollingScreen.kt
@@ -3,7 +3,10 @@ package com.poddlybonk.jetfundamentals.screens
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
@@ -11,6 +14,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.poddlybonk.jetfundamentals.R
 import com.poddlybonk.jetfundamentals.router.BackButtonHandler
 import com.poddlybonk.jetfundamentals.router.JetFundamentalsRouter
 import com.poddlybonk.jetfundamentals.router.Screen
@@ -25,8 +29,12 @@ fun ScrollingScreen() {
 }
 
 @Composable
-fun MyScrollingScreen() {
-    // TODO add your code here
+fun MyScrollingScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+        BookImage(R.drawable.advanced_architecture_android, R.string.advanced_architecture_android)
+        BookImage(R.drawable.kotlin_aprentice, R.string.kotlin_apprentice)
+        BookImage(R.drawable.kotlin_coroutines, R.string.kotlin_coroutines)
+    }
 }
 
 @Composable
