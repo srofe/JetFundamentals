d59c430f047fbb84815717b1221ddbb0eacd376a
Simon Rofe <simon.rofe@ieee.org>
2022-06-23 11:39:27 +1000

    Add horizontal row of items

--- app/src/main/java/com/poddlybonk/jetfundamentals/screens/RowScreen.kt
+++ app/src/main/java/com/poddlybonk/jetfundamentals/screens/RowScreen.kt
@@ -1,6 +1,14 @@
 package com.poddlybonk.jetfundamentals.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import com.poddlybonk.jetfundamentals.R
 import com.poddlybonk.jetfundamentals.router.BackButtonHandler
 import com.poddlybonk.jetfundamentals.router.JetFundamentalsRouter
@@ -19,5 +27,15 @@ fun RowScreen() {
 
 @Composable
 fun MyRow() {
-    // TODO write your code here
+    Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        THREE_ELEMENT_LIST.forEach { textResId -> 
+            Text(
+                text = stringResource(id = textResId),
+                fontSize = 18.sp
+            )
+        }
+    }
 }
