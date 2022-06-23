4cf1fa9461ed7814e953329638378aa58552bda8
Simon Rofe <simon.rofe@ieee.org>
2022-06-23 13:34:27 +1000

    Add box layout

--- app/src/main/java/com/poddlybonk/jetfundamentals/screens/BoxScreen.kt
+++ app/src/main/java/com/poddlybonk/jetfundamentals/screens/BoxScreen.kt
@@ -1,7 +1,15 @@
 package com.poddlybonk.jetfundamentals.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import com.poddlybonk.jetfundamentals.R
 import com.poddlybonk.jetfundamentals.router.BackButtonHandler
 import com.poddlybonk.jetfundamentals.router.JetFundamentalsRouter
 import com.poddlybonk.jetfundamentals.router.Screen
@@ -16,6 +24,25 @@ fun BoxScreen() {
 }
 
 @Composable
-fun MyBox(modifier: Modifier = Modifier) {
-    // TODO write your code here
+fun MyBox(
+    modifier: Modifier = Modifier,
+    contentModifier: Modifier = Modifier
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Text(
+            text = stringResource(id = R.string.first),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.TopStart)
+        )
+        Text(
+            text = stringResource(id = R.string.second),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.Center)
+        )
+        Text(
+            text = stringResource(id = R.string.third),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.BottomEnd)
+        )
+    }
 }
