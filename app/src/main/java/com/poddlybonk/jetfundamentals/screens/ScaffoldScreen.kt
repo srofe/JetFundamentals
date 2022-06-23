7381d76712e082349cabff12e05f8a8721ed1274
Simon Rofe <simon.rofe@ieee.org>
2022-06-23 13:56:08 +1000

    Add scaffold layout

--- app/src/main/java/com/poddlybonk/jetfundamentals/screens/ScaffoldScreen.kt
+++ app/src/main/java/com/poddlybonk/jetfundamentals/screens/ScaffoldScreen.kt
@@ -1,10 +1,16 @@
 package com.poddlybonk.jetfundamentals.screens
 
+import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.res.colorResource
+import com.poddlybonk.jetfundamentals.R
 import com.poddlybonk.jetfundamentals.router.BackButtonHandler
 import com.poddlybonk.jetfundamentals.router.JetFundamentalsRouter
 import com.poddlybonk.jetfundamentals.router.Screen
+import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun ScaffoldScreen() {
@@ -17,11 +23,21 @@ fun ScaffoldScreen() {
 
 @Composable
 fun MyScaffold() {
-    // TODO write your code here
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val scope: CoroutineScope = rememberCoroutineScope()
+    
+    Scaffold(
+        scaffoldState = scaffoldState,
+        contentColor = colorResource(id = R.color.colorPrimary),
+        content = { MyRow() },
+        topBar = { MyTopAppBar(scaffoldState = scaffoldState, scope = scope) },
+        bottomBar = { MyBottomAppBar() },
+        drawerContent = { MyColumn() }
+    )
 }
 
 @Composable
-fun MyTopAppBar(scaffoldState: ScaffoldState) {
+fun MyTopAppBar(scaffoldState: ScaffoldState, scope: CoroutineScope) {
     // TODO write your code here
 }
 
